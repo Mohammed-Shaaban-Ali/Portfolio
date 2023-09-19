3b98b523647edbaf3f4aa9afbbc19b1014a21173
Mohammed Shaaban <ms7500746@gnail.com>
2023-09-19 12:42:54 +0300

    sckills

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -13,7 +13,7 @@ const SectionTitle = (props) => {
         initial={{ x: -350 }}
         whileInView={{ x: 0 }}
         transition={{ duration: 0.6, type: "spring" }}
-        className="text-primary-600 p-6 noselect"
+        className="text-primary-500 p-6 noselect"
       >
         <span
           className="opacity-50"
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
+import MarqueeCards from "../components/MarqueeCards";
+import SectionTitle from "../components/SectionTitle";
+import SkillsCards from "../components/SkillsCards";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const { t } = useTranslation();
+  const title = t("skillsSction");
+  const subtitle = t("skillsSubtitle");
+  return (
+    <div
+      id="skills"
+      className="relative w-full overflow-hidden-web flex justify-center"
+    >
+      <div className="w-full min-h-[800px] flex flex-col xl:w-[70%]">
+        <div className="blur blurskills"></div>
+        <div className="blur blurskills2"></div>
+        <div className="w-full">
+          <SectionTitle title={title} subtitle={subtitle} />
+        </div>
+        <div className="xl:border-l-2 xl:border-r-2 xl:border-primary-400 h-full dirction">
+          <div className="relative">
+            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <MarqueeCards direction="left">
+              <SkillsCards />
+            </MarqueeCards>
+          </div>
+          <div className="relative">
+            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <MarqueeCards direction="right">
+              <SkillsCards />
+            </MarqueeCards>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
